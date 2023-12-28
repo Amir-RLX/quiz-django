4a41d51052220e33cc9afb545abe1e26f58c1db5
Amir-RLX <amir.rahati@gmail.com>
2023-12-28 13:34:45 +0330

    adding uuid and updating quiz/models

--- quizsite/quiz/models.py
+++ quizsite/quiz/models.py
@@ -1,7 +1,16 @@
 from django.db import models
+import uuid
 
 
 # Create your models here.
+class Quiz(models.Model):
+    uuid = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True)
+    title = models.TextField(editable=True)
+    img = models.ImageField(editable=True)
+    results = models.ForeignKey('Result', on_delete=models.PROTECT)
+    questions = models.ForeignKey('Question', on_delete=models.PROTECT)
+
+
 class Result(models.Model):
     title = models.TextField(editable=True)
     img = models.ImageField(editable=True)
@@ -14,4 +23,6 @@ class Question(models.Model):
 
 
 class Choices(models.Model):
-    ...
+    title = models.TextField(editable=True)
+    question = models.ForeignKey('Question', on_delete=models.PROTECT)
+    result = models.ForeignKey('Result', on_delete=models.PROTECT)
