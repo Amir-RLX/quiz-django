b5ef73246a8bd6f715038e1a0c150b8f1eb9f8b6
Amir-RLX <amir.rahati@gmail.com>
2023-12-29 14:05:14 +0330

    edited models

--- quizsite/quiz/admin.py
+++ quizsite/quiz/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from . import models
+
 
 # Register your models here.
+
+class QuizAdmin(admin.ModelAdmin):
+    readonly_fields = ('uuid',)
+    list_display = ('title','published','not_published')
+    search_fields = ('title',)
+
+
+class ResultAdmin(admin.ModelAdmin):
+    search_fields = ('title',)
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    search_fields = ('title',)
+
+
+class ChoicesAdmin(admin.ModelAdmin):
+    search_fields = ('title',)
+
+
+admin.site.register(models.Quiz, QuizAdmin)
+admin.site.register(models.Result, ResultAdmin)
+admin.site.register(models.Question, QuestionAdmin)
+admin.site.register(models.Choices, ChoicesAdmin)
--- quizsite/quiz/models.py
+++ quizsite/quiz/models.py
@@ -7,22 +7,36 @@ class Quiz(models.Model):
     uuid = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True)
     title = models.TextField(editable=True)
     img = models.ImageField(editable=True)
-    results = models.ForeignKey('Result', on_delete=models.PROTECT)
-    questions = models.ForeignKey('Question', on_delete=models.PROTECT)
+    published = models.BooleanField(verbose_name="Published", default=False)
+    not_published = models.BooleanField(verbose_name="NOT Published", default=False)
+
+    def __str__(self):
+        return self.title
 
 
 class Result(models.Model):
     title = models.TextField(editable=True)
     img = models.ImageField(editable=True)
     description = models.TextField(editable=True)
+    quiz = models.ForeignKey('Quiz', on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.title
 
 
 class Question(models.Model):
     title = models.TextField(editable=True)
     img = models.ImageField(editable=True)
+    quiz = models.ForeignKey('Quiz', on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.title
 
 
 class Choices(models.Model):
     title = models.TextField(editable=True)
     question = models.ForeignKey('Question', on_delete=models.PROTECT)
     result = models.ForeignKey('Result', on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.title
